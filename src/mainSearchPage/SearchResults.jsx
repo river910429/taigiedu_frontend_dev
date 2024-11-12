fd48ed500b4d4c18c642b95e859271762b395ebc
hsiehyoung <p96111119@gs.ncku.edu.tw>
2024-11-12 21:13:17 +0800

    還沒做完的搜尋

--- src/mainSearchPage/SearchResults.jsx
+++ src/mainSearchPage/SearchResults.jsx
@@ -3,32 +3,336 @@ import './SearchResults.css';
 
 const SearchResults = ({ resultsLeft, resultsRight }) => {
   return (
-    <div className="search-results">
-      <div className="results-table">
-        <div className="table-header">
-          <span>資料來源</span>
-          <span>內容</span>
-        </div>
-        {resultsLeft.map((result, index) => (
-          <div className="table-row" key={index}>
-            <span>{result.source}</span>
-            <span>{result.content}</span>
+    <div>
+      <div className="row pt-5 px-0">
+        <div className="col-6">
+          <div className="row shadow px-3 py-3 titleCard cardContainer">
+            <div className="col-3 p-0 ">資源出處</div>
+            <div className="col-9 p-0 ">內容</div>
           </div>
-        ))}
+          <a
+            href="https://taigi.page//posts/%E5%8F%B0%E8%AA%9E-%E8%A2%82%E4%BD%BF%E5%8F%AB%E5%81%9A-%E5%8F%B0%E8%AA%9E/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="row px-3 py-3 sentenceCard cardContainer">
+              <div className="col-3 p-0 sentenceTitle">潘科元台語文理想國</div>
+              <div className="col-9 p-0 sentenceContent">
+                真濟儂不准阮稱呼家己个語言是<b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>，怹个理由是：原住民、客家人[hag2 ga24 ngin11] 嘛有權利稱呼家己个語言是
+                <b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>，是按怎恁有權獨佔即个名稱？阮嘛歡迎原住民、客儂稱呼家己个話是
+                <b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>。
+              </div>
+            </div>
+          </a>
+          <a
+            href="https://taigi.page//posts/%E5%8F%B0%E8%AA%9E-%E8%A2%82%E4%BD%BF%E5%8F%AB%E5%81%9A-%E5%8F%B0%E8%AA%9E/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="row px-3 py-3 sentenceCard cardContainer">
+              <div className="col-3 p-0 sentenceTitle">潘科元台語文理想國</div>
+              <div className="col-9 p-0 sentenceContent">
+                真濟儂不准阮稱呼家己个語言是<b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>，怹个理由是：原住民、客家人[hag2 ga24 ngin11] 嘛有權利稱呼家己个語言是
+                <b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>，是按怎恁有權獨佔即个名稱？阮嘛歡迎原住民、客儂稱呼家己个話是
+                <b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>。
+              </div>
+            </div>
+          </a>
+          <a
+            href="https://taigi.page//posts/%E5%8F%B0%E8%AA%9E-%E8%A2%82%E4%BD%BF%E5%8F%AB%E5%81%9A-%E5%8F%B0%E8%AA%9E/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="row px-3 py-3 sentenceCard cardContainer">
+              <div className="col-3 p-0 sentenceTitle">潘科元台語文理想國</div>
+              <div className="col-9 p-0 sentenceContent">
+                真濟儂不准阮稱呼家己个語言是<b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>，怹个理由是：原住民、客家人[hag2 ga24 ngin11] 嘛有權利稱呼家己个語言是
+                <b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>，是按怎恁有權獨佔即个名稱？阮嘛歡迎原住民、客儂稱呼家己个話是
+                <b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>。
+              </div>
+            </div>
+          </a>
+          <a
+            href="https://taigi.page//posts/%E5%8F%B0%E8%AA%9E-%E8%A2%82%E4%BD%BF%E5%8F%AB%E5%81%9A-%E5%8F%B0%E8%AA%9E/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="row px-3 py-3 sentenceCard cardContainer">
+              <div className="col-3 p-0 sentenceTitle">潘科元台語文理想國</div>
+              <div className="col-9 p-0 sentenceContent">
+                真濟儂不准阮稱呼家己个語言是<b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>，怹个理由是：原住民、客家人[hag2 ga24 ngin11] 嘛有權利稱呼家己个語言是
+                <b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>，是按怎恁有權獨佔即个名稱？阮嘛歡迎原住民、客儂稱呼家己个話是
+                <b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>。
+              </div>
+            </div>
+          </a>
+          <a
+            href="https://taigi.page//posts/%E5%8F%B0%E8%AA%9E-%E8%A2%82%E4%BD%BF%E5%8F%AB%E5%81%9A-%E5%8F%B0%E8%AA%9E/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="row px-3 py-3 sentenceCard cardContainer">
+              <div className="col-3 p-0 sentenceTitle">潘科元台語文理想國</div>
+              <div className="col-9 p-0 sentenceContent">
+                真濟儂不准阮稱呼家己个語言是<b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>，怹个理由是：原住民、客家人[hag2 ga24 ngin11] 嘛有權利稱呼家己个語言是
+                <b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>，是按怎恁有權獨佔即个名稱？阮嘛歡迎原住民、客儂稱呼家己个話是
+                <b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>。
+              </div>
+            </div>
+          </a>
+          <a
+            href="https://taigi.page//posts/%E5%8F%B0%E8%AA%9E-%E8%A2%82%E4%BD%BF%E5%8F%AB%E5%81%9A-%E5%8F%B0%E8%AA%9E/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="row px-3 py-3 sentenceCard cardContainer">
+              <div className="col-3 p-0 sentenceTitle">潘科元台語文理想國</div>
+              <div className="col-9 p-0 sentenceContent">
+                真濟儂不准阮稱呼家己个語言是<b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>，怹个理由是：原住民、客家人[hag2 ga24 ngin11] 嘛有權利稱呼家己个語言是
+                <b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>，是按怎恁有權獨佔即个名稱？阮嘛歡迎原住民、客儂稱呼家己个話是
+                <b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>。
+              </div>
+            </div>
+          </a>
+          <a
+            href="https://taigi.page//posts/%E5%8F%B0%E8%AA%9E-%E8%A2%82%E4%BD%BF%E5%8F%AB%E5%81%9A-%E5%8F%B0%E8%AA%9E/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="row px-3 py-3 sentenceCard cardContainer">
+              <div className="col-3 p-0 sentenceTitle">潘科元台語文理想國</div>
+              <div className="col-9 p-0 sentenceContent">
+                真濟儂不准阮稱呼家己个語言是<b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>，怹个理由是：原住民、客家人[hag2 ga24 ngin11] 嘛有權利稱呼家己个語言是
+                <b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>，是按怎恁有權獨佔即个名稱？阮嘛歡迎原住民、客儂稱呼家己个話是
+                <b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>。
+              </div>
+            </div>
+          </a>
+          <a
+            href="https://taigi.page//posts/%E5%8F%B0%E8%AA%9E-%E8%A2%82%E4%BD%BF%E5%8F%AB%E5%81%9A-%E5%8F%B0%E8%AA%9E/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="row px-3 py-3 sentenceCard cardContainer">
+              <div className="col-3 p-0 sentenceTitle">潘科元台語文理想國</div>
+              <div className="col-9 p-0 sentenceContent">
+                真濟儂不准阮稱呼家己个語言是<b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>，怹个理由是：原住民、客家人[hag2 ga24 ngin11] 嘛有權利稱呼家己个語言是
+                <b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>，是按怎恁有權獨佔即个名稱？阮嘛歡迎原住民、客儂稱呼家己个話是
+                <b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>。
+              </div>
+            </div>
+          </a>
+        </div>
+        <div className="col-6">
+          <div className="row shadow px-3 py-3 titleCard cardContainer">
+            <div className="col-3 p-0 ">資源出處</div>
+            <div className="col-9 p-0 ">內容</div>
+          </div>
+          <a
+            href="https://taigi.page//posts/%E5%8F%B0%E8%AA%9E-%E8%A2%82%E4%BD%BF%E5%8F%AB%E5%81%9A-%E5%8F%B0%E8%AA%9E/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="row px-3 py-3 sentenceCard cardContainer">
+              <div className="col-3 p-0 sentenceTitle">潘科元台語文理想國</div>
+              <div className="col-9 p-0 sentenceContent">
+                真濟儂不准阮稱呼家己个語言是<b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>，怹个理由是：原住民、客家人[hag2 ga24 ngin11] 嘛有權利稱呼家己个語言是
+                <b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>，是按怎恁有權獨佔即个名稱？阮嘛歡迎原住民、客儂稱呼家己个話是
+                <b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>。
+              </div>
+            </div>
+          </a>
+          <a
+            href="https://taigi.page//posts/%E5%8F%B0%E8%AA%9E-%E8%A2%82%E4%BD%BF%E5%8F%AB%E5%81%9A-%E5%8F%B0%E8%AA%9E/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="row px-3 py-3 sentenceCard cardContainer">
+              <div className="col-3 p-0 sentenceTitle">潘科元台語文理想國</div>
+              <div className="col-9 p-0 sentenceContent">
+                真濟儂不准阮稱呼家己个語言是<b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>，怹个理由是：原住民、客家人[hag2 ga24 ngin11] 嘛有權利稱呼家己个語言是
+                <b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>，是按怎恁有權獨佔即个名稱？阮嘛歡迎原住民、客儂稱呼家己个話是
+                <b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>。
+              </div>
+            </div>
+          </a>
+          <a
+            href="https://taigi.page//posts/%E5%8F%B0%E8%AA%9E-%E8%A2%82%E4%BD%BF%E5%8F%AB%E5%81%9A-%E5%8F%B0%E8%AA%9E/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="row px-3 py-3 sentenceCard cardContainer">
+              <div className="col-3 p-0 sentenceTitle">潘科元台語文理想國</div>
+              <div className="col-9 p-0 sentenceContent">
+                真濟儂不准阮稱呼家己个語言是<b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>，怹个理由是：原住民、客家人[hag2 ga24 ngin11] 嘛有權利稱呼家己个語言是
+                <b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>，是按怎恁有權獨佔即个名稱？阮嘛歡迎原住民、客儂稱呼家己个話是
+                <b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>。
+              </div>
+            </div>
+          </a>
+          <a
+            href="https://taigi.page//posts/%E5%8F%B0%E8%AA%9E-%E8%A2%82%E4%BD%BF%E5%8F%AB%E5%81%9A-%E5%8F%B0%E8%AA%9E/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="row px-3 py-3 sentenceCard cardContainer">
+              <div className="col-3 p-0 sentenceTitle">潘科元台語文理想國</div>
+              <div className="col-9 p-0 sentenceContent">
+                真濟儂不准阮稱呼家己个語言是<b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>，怹个理由是：原住民、客家人[hag2 ga24 ngin11] 嘛有權利稱呼家己个語言是
+                <b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>，是按怎恁有權獨佔即个名稱？阮嘛歡迎原住民、客儂稱呼家己个話是
+                <b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>。
+              </div>
+            </div>
+          </a>
+          <a
+            href="https://taigi.page//posts/%E5%8F%B0%E8%AA%9E-%E8%A2%82%E4%BD%BF%E5%8F%AB%E5%81%9A-%E5%8F%B0%E8%AA%9E/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="row px-3 py-3 sentenceCard cardContainer">
+              <div className="col-3 p-0 sentenceTitle">潘科元台語文理想國</div>
+              <div className="col-9 p-0 sentenceContent">
+                真濟儂不准阮稱呼家己个語言是<b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>，怹个理由是：原住民、客家人[hag2 ga24 ngin11] 嘛有權利稱呼家己个語言是
+                <b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>，是按怎恁有權獨佔即个名稱？阮嘛歡迎原住民、客儂稱呼家己个話是
+                <b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>。
+              </div>
+            </div>
+          </a>
+          <a
+            href="https://taigi.page//posts/%E5%8F%B0%E8%AA%9E-%E8%A2%82%E4%BD%BF%E5%8F%AB%E5%81%9A-%E5%8F%B0%E8%AA%9E/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="row px-3 py-3 sentenceCard cardContainer">
+              <div className="col-3 p-0 sentenceTitle">潘科元台語文理想國</div>
+              <div className="col-9 p-0 sentenceContent">
+                真濟儂不准阮稱呼家己个語言是<b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>，怹个理由是：原住民、客家人[hag2 ga24 ngin11] 嘛有權利稱呼家己个語言是
+                <b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>，是按怎恁有權獨佔即个名稱？阮嘛歡迎原住民、客儂稱呼家己个話是
+                <b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>。
+              </div>
+            </div>
+          </a>
+          <a
+            href="https://taigi.page//posts/%E5%8F%B0%E8%AA%9E-%E8%A2%82%E4%BD%BF%E5%8F%AB%E5%81%9A-%E5%8F%B0%E8%AA%9E/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="row px-3 py-3 sentenceCard cardContainer">
+              <div className="col-3 p-0 sentenceTitle">潘科元台語文理想國</div>
+              <div className="col-9 p-0 sentenceContent">
+                真濟儂不准阮稱呼家己个語言是<b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>，怹个理由是：原住民、客家人[hag2 ga24 ngin11] 嘛有權利稱呼家己个語言是
+                <b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>，是按怎恁有權獨佔即个名稱？阮嘛歡迎原住民、客儂稱呼家己个話是
+                <b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>。
+              </div>
+            </div>
+          </a>
+          <a
+            href="https://taigi.page//posts/%E5%8F%B0%E8%AA%9E-%E8%A2%82%E4%BD%BF%E5%8F%AB%E5%81%9A-%E5%8F%B0%E8%AA%9E/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="row px-3 py-3 sentenceCard cardContainer">
+              <div className="col-3 p-0 sentenceTitle">潘科元台語文理想國</div>
+              <div className="col-9 p-0 sentenceContent">
+                真濟儂不准阮稱呼家己个語言是<b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>，怹个理由是：原住民、客家人[hag2 ga24 ngin11] 嘛有權利稱呼家己个語言是
+                <b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>，是按怎恁有權獨佔即个名稱？阮嘛歡迎原住民、客儂稱呼家己个話是
+                <b style={{ color: 'red' }}>台</b>
+                <b style={{ color: 'red' }}>語</b>。
+              </div>
+            </div>
+          </a>
+        </div>
       </div>
 
-      <div className="results-table">
-        <div className="table-header">
-          <span>資料來源</span>
-          <span>內容</span>
-        </div>
-        {resultsRight.map((result, index) => (
-          <div className="table-row" key={index}>
-            <span>{result.source}</span>
-            <span>{result.content}</span>
-          </div>
-        ))}
-      </div>
+      <ul className="pagination">
+        <li className="page-item"><a className="page-link wide-link" href="javascript:previousPage();">《 Back</a></li>
+        <li className="page-item"><a className="page-link" href="javascript:changePage(8);">8</a></li>
+        <li className="page-item"><a className="page-link" href="javascript:changePage(9);">9</a></li>
+        <li className="page-item"><a className="page-link" href="javascript:changePage(10);">10</a></li>
+        <li className="page-item"><a className="page-link" href="javascript:changePage(11);">11</a></li>
+        <li className="page-item active" aria-current="page"><a className="page-link">12</a></li>
+        <li className="page-item"><a className="page-link" href="javascript:changePage(13);">13</a></li>
+        <li className="page-item"><a className="page-link" href="javascript:changePage(14);">14</a></li>
+        <li className="page-item"><a className="page-link" href="javascript:changePage(15);">15</a></li>
+        <li className="page-item"><a className="page-link" href="javascript:changePage(16);">16</a></li>
+        <li className="page-item"><a className="page-link" href="javascript:changePage(17);">17</a></li>
+        <li className="page-item"><a className="page-link wide-link" href="javascript:nextPage();">》Next</a></li>
+      </ul>
+      {/* 你可以在這裡加入其他的結果列表 */}
+      {/* <div className="search-results"> */}
+      {/*   <div className="results-table"> */}
+      {/*     <div className="table-header"> */}
+      {/*       <span>資料來源</span> */}
+      {/*       <span>內容</span> */}
+      {/*     </div> */}
+      {/*     {resultsLeft.map((result, index) => ( */}
+      {/*       <div className="table-row" key={index}> */}
+      {/*         <span>{result.source}</span> */}
+      {/*         <span>{result.content}</span> */}
+      {/*       </div> */}
+      {/*     ))} */}
+      {/*   </div> */}
+
+      {/*   <div className="results-table"> */}
+      {/*     <div className="table-header"> */}
+      {/*       <span>資料來源</span> */}
+      {/*       <span>內容</span> */}
+      {/*     </div> */}
+      {/*     {resultsRight.map((result, index) => ( */}
+      {/*       <div className="table-row" key={index}> */}
+      {/*         <span>{result.source}</span> */}
+      {/*         <span>{result.content}</span> */}
+      {/*       </div> */}
+      {/*     ))} */}
+      {/*   </div> */}
+      {/* </div> */}
     </div>
   );
 };
