5c097d2430546ff557544f765c2197093ec17772
chen-yenan <407631356@gms.tku.edu.tw>
2024-12-13 17:33:08 +0800

    改translatePage資料夾名稱!

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,8 @@ import MainContent from "./MainContent";
 import MainSearchPage from './mainSearchPage/MainSearchPage.jsx';
 import TranscriptPage from './transcriptPage/TranscriptPage.jsx';
 import ReadPage from './readPage/ReadPage.jsx';
-import TranslatePage from './translate/TranslatePage.jsx';
+import TranslatePage from './translatePage/TranslatePage.jsx';
+import ResourcePage from './translatePage/TranslatePage.jsx';
 
 const App = () => {
   return (
@@ -24,6 +25,7 @@ const App = () => {
             <Route path="/transcript" element={<TranscriptPage />} />
             <Route path="/read" element={<ReadPage />} />
             <Route path="/translate" element={<TranslatePage />} />
+            <Route path="/resource" element={<ResourcePage />} />
           </Routes>
         </div>
         <Footer />
