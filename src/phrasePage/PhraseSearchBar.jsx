dd8d85f5b105873d3c87104854d846708b6e4f8d
Keywind <watersprayer127@gmail.com>
2025-02-08 14:58:31 +0800

    fix: correct jsx/css import typos
    Fixed filename letter case mismatches.

--- src/phrasePage/PhraseSearchBar.jsx
+++ src/phrasePage/PhraseSearchBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import MultiSelect from './MultiSelect';
+import MultiSelect from './multiselect';
 import './PhraseSearchBar.css';
 
 const PhraseSearchBar = () => {
