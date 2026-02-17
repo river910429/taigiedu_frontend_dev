39f9dd87f62e23b414f0b5b81b26ca493b067838
ncchen99 <ncchen99@gmail.com>
2026-02-17 23:24:07 +0800

    feat: add Footer component with navigation links and copyright information.

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -15,7 +15,6 @@ const Footer = () => {
         <button className="footer-button">團隊介紹</button>
         <button className="footer-button" onClick={() => handleLinkClick('terms')}>使用條款</button>
         <button className="footer-button" onClick={() => handleLinkClick('policy')}>隱私政策</button>
-        <button className="footer-button" onClick={() => handleLinkClick('admin')}>管理後台</button>
       </div>
       <div className="footer-text">
         © 2024 台語文教學共融平台 All Rights Reserved.
